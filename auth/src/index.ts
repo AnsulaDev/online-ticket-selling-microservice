ec48d48aa2a6b61fab78930ad05a34d7db7815f0
AnsulaDev <ansulaoari88@gmail.com>
2023-07-09 21:15:14 +0530

    adding routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import{json} from 'body-parser';
+import { currntUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutrRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 
 app.use(json());
+
+app.use(currntUserRouter);
+app.use(signinRouter);
+app.use(signoutrRouter);
+app.use(signupRouter);
 
 app.listen(3000, ()=>{
     console.log('Listening on port 3000!!!!')
