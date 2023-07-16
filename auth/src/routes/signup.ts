113ae47e42740eb72c9714ea81ebcb6814a5b4ac
AnsulaDev <ansulaoari88@gmail.com>
2023-07-16 21:24:11 +0530

    added mongodb and also added custom logic to solve to mongoose and typeScript  type checking

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-contection-error';
+
 const router = express.Router();
 
 router.post('/api/users/signup',[
@@ -18,12 +19,16 @@ router.post('/api/users/signup',[
     if(!errors.isEmpty()){ //This line checks if the errors object is not empty, indicating that there are validation errors.
         throw new RequestValidationError(errors.array());
     }
-    const {email , password} = req.body;
-
-    console.log('creating a user....');
-    throw new DatabaseConnectionError();
-    res.send({})
     
+    const {email, password} =req.body;
+    const existingUser = await User.findOne({ email});
+    if( existingUser ){
+        console.log('Email is already used');
+        return res.send({});
+    }
+    const user = User.build({email, password});
+    await user.save();
+    res.status(201).send(user);
 });
 
 export { router as signupRouter};
